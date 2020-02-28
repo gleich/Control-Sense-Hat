a22b80b7567e911a443a99642280fdf5a5c280e0
Matthew Gleich <matthewgleich@gmail.com>
2020-02-28 17:40:55 -0500

    Use user created files

--- control-sense-hat/main.py
+++ control-sense-hat/main.py
@@ -4,7 +4,6 @@ from os import system
 import json
 from time import sleep
 
-system("sudo chmod ugo+rwx sense_hat_containerized")
 os.chdir("./sense_hat_containerized")
 # Writing templates
 data_template = {
@@ -16,12 +15,6 @@ data_template = {
 leds_template = []
 for i in range(64):
     leds_template.append((0, 0, 0))
-system("rm -rf data.json")
-system("rm -rf leds.json")
-system("touch data.json")
-system("touch leds.json")
-system("sudo chmod ugo+rwx data.json")
-system("sudo chmod ugo+rwx leds.json")
 with open("data.json", "w") as data_json_init:
     json.dump(data_template, data_json_init)
 with open("leds.json", "w") as leds_json_init:
---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd ~
+
+mkdir sense_hat_containerized
+
+cd sense_hat_containerized
+
+touch data.json
+
+touch leds.json
